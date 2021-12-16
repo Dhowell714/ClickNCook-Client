058b15c0ac00d334d696e27479d47e48a52028bd
Luna Silver <drhowell714@gmail.com>
2021-12-16 18:07:48 -0500

    d

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -2,13 +2,13 @@ let APIURL = '';
 
 switch (window.location.hostname) {
 
-    case 'localhost' || '127.0.0.1':
-        APIURL = 'http://localhost:3000';
-        break;
+    // case 'localhost' || '127.0.0.1':
+    //     APIURL = 'http://localhost:3000';
+    //     break;
 
-        case 'dj-react-click-n-cook.herokuapp.com':
+        case 'https://dj-cooking-app.herokuapp.com':
 
-        APIURL = 'https://dj-react-click-n-cook.herokuapp.com'
+        APIURL = 'https://dj-cooking-app.herokuapp.com'
 }
 
 export default APIURL;
